18131f7632efb47867bea07089dfb57cb15d81ac
Aparna1-ag <aparnagupta.csa@gmail.com>
2025-12-23 18:35:16 +0530

    formatted

--- index.js
+++ index.js
@@ -3,46 +3,33 @@ const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 const { google } = require("googleapis");
-require("dotenv").config()
+require("dotenv").config();
 const mongoose = require("mongoose");
-const cors = require('cors')
-const authMiddleware =  require('./middleware/authMiddleware')
+const cors = require("cors");
+const authMiddleware = require("./middleware/authMiddleware");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const cookieParser =  require("cookie-parser")
-
-
-
-
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-
-
-app.use(cookieParser())
+app.use(cookieParser());
 
 // console.log(authMiddleware)
 
-
-
-app.use(cors(
-  {
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
+  })
+);
 
-  }
-
-))
-
-app.use(express.json())
-
+app.use(express.json());
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/community-chat")
   .then(() => console.log("MongoDB Connected"))
   .catch(console.error);
-
-
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -52,23 +39,7 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  // files: [
-  //   {
-  //     _id: {type: mongoose.Schema.Types.ObjectId, auto: true},
-  //     name: String,
-  //     url: String,
-  //     type: {
-  //       type: String,
-  //       enum: ["image", "video", "document"]
-  //     },
-  //     createdAt: { type: Date, default: Date.now },
-  //     mimeType: String,
-
-  //   }
-  // ]
-
 });
-
 
 app.post("/api/auth/login", async (req, res) => {
   const { email, password } = req.body;
@@ -91,13 +62,11 @@ app.post("/api/auth/login", async (req, res) => {
     return res.status(401).json({ message: "Wrong password" });
   }
 
-
-
   const token = jwt.sign(
     {
       id: user._id,
       email: user.email,
-      name: user.name
+      name: user.name,
     },
     process.env.ACCESS_TOKEN_SECRET,
     { expiresIn: "7d" }
@@ -107,47 +76,40 @@ app.post("/api/auth/login", async (req, res) => {
   // await user.save();
 
   res.cookie("session", token, {
-  httpOnly: true,
-  secure: false,        
-  sameSite: "lax",   
-  maxAge: 24 * 60 * 60 * 1000
-  })
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
 
- res.json({
+  res.json({
     auth: true,
     token,
     user: {
       _id: user._id,
       name: user.name,
-      email: user.email
-    }
+      email: user.email,
+    },
   });
 });
 
-
-
-
 const fileSchema = new mongoose.Schema({
-  
-    _id: {type: mongoose.Schema.Types.ObjectId, auto: true},
-    name: String,
-    url: String,
-    type: {
-      type: String,
-      enum: ["image", "video", "document"]
-    },
-    createdAt: { type: Date, default: Date.now },
-    size: Number,
-    mimeType: String,
-    user: {type: mongoose.Schema.Types.ObjectId, ref: "User"}
-
-  
-
-})
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+  name: String,
+  url: String,
+  type: {
+    type: String,
+    enum: ["image", "video", "document"],
+  },
+  createdAt: { type: Date, default: Date.now },
+  size: Number,
+  mimeType: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
 
 const User = mongoose.model("User", userSchema);
 
-const File = mongoose.model("File", fileSchema)
+const File = mongoose.model("File", fileSchema);
 
 const upload = multer({ dest: "uploads/" });
 
@@ -164,9 +126,11 @@ const drive = google.drive({
   auth,
 });
 
-const uploadMiddleWare = upload.fields([{name: "photos", maxCount: 10}, {name: "videos", maxCount: 5}, {name: "documents", maxCount: 10}])
-
-
+const uploadMiddleWare = upload.fields([
+  { name: "photos", maxCount: 10 },
+  { name: "videos", maxCount: 5 },
+  { name: "documents", maxCount: 10 },
+]);
 
 app.post("/uploadfile/:userId", uploadMiddleWare, async (req, res) => {
   // console.log(req.files)
@@ -175,13 +139,7 @@ app.post("/uploadfile/:userId", uploadMiddleWare, async (req, res) => {
     return res.status(400).json({ error: "No files uploaded" });
   }
 
-  const userId = req.params.userId
-
- 
-
-
-
-
+  const userId = req.params.userId;
 
   try {
     await drive.files.get({
@@ -189,117 +147,109 @@ app.post("/uploadfile/:userId", uploadMiddleWare, async (req, res) => {
       supportsAllDrives: true,
     });
 
+    // const photoIds = []
+    // const videoIds = []
+    // const documentIds = []
 
-    
+    const allFiles = [];
 
-// const photoIds = []
-// const videoIds = []
-// const documentIds = []
+    if (req.files.photos?.length) {
+      for (let each of req.files.photos) {
+        const response = await drive.files.create({
+          requestBody: {
+            name: each.originalname,
+            parents: [process.env.PARENT_ID],
+          },
+          media: {
+            mimeType: each.mimetype,
+            body: fs.createReadStream(each.path),
+          },
+          supportsAllDrives: true,
+          fields: "id, name",
+        });
 
-const allFiles = []
-
-  if (req.files.photos?.length) {
-    for (let each of req.files.photos ) {
-
-     const response = await drive.files.create({
-        requestBody: {
-          name: each.originalname,
-          parents: [process.env.PARENT_ID],
-        },
-        media: {
+        allFiles.push({
+          url: response.data.id,
+          name: response.data.name,
           mimeType: each.mimetype,
-          body: fs.createReadStream(each.path),
-        },
-        supportsAllDrives: true,
-        fields: "id, name",
-      });
+          size: each.size,
+          type: "image",
+          user: userId,
+        });
 
-      allFiles.push({url: response.data.id, name: response.data.name, mimeType: each.mimetype, size: each.size, type: "image", user: userId})
-      
-      fs.unlinkSync(each.path);
-
+        fs.unlinkSync(each.path);
+      }
     }
 
+    if (req.files.videos?.length) {
+      for (let each of req.files.videos) {
+        const response = await drive.files.create({
+          requestBody: {
+            name: each.originalname,
+            parents: [process.env.PARENT_ID],
+          },
+          media: {
+            mimeType: each.mimetype,
+            body: fs.createReadStream(each.path),
+          },
+          supportsAllDrives: true,
+          fields: "id, name",
+        });
 
-
-  }
-
-
-  if (req.files.videos?.length) {
-    for (let each of req.files.videos ) {
-
-      const response = await drive.files.create({
-        requestBody: {
-          name: each.originalname,
-          parents: [process.env.PARENT_ID],
-        },
-        media: {
+        allFiles.push({
+          url: response.data.id,
+          name: response.data.name,
           mimeType: each.mimetype,
-          body: fs.createReadStream(each.path),
-        },
-        supportsAllDrives: true,
-        fields: "id, name",
-      });
+          size: each.size,
+          type: "video",
+          user: userId,
+        });
 
-      allFiles.push({url: response.data.id, name: response.data.name, mimeType: each.mimetype, size: each.size, type: "video", user: userId})
-
-      fs.unlinkSync(each.path);
-
-      
+        fs.unlinkSync(each.path);
+      }
     }
-  }
 
+    if (req.files.documents?.length) {
+      for (let each of req.files.documents) {
+        const response = await drive.files.create({
+          requestBody: {
+            name: each.originalname,
+            parents: [process.env.PARENT_ID],
+          },
+          media: {
+            mimeType: each.mimetype,
+            body: fs.createReadStream(each.path),
+          },
+          supportsAllDrives: true,
+          fields: "id, name",
+        });
 
-  if (req.files.documents?.length) {
-    for (let each of req.files.documents ) {
-
-      const response = await drive.files.create({
-        requestBody: {
-          name: each.originalname,
-          parents: [process.env.PARENT_ID],
-        },
-        media: {
+        allFiles.push({
+          url: response.data.id,
+          name: response.data.name,
           mimeType: each.mimetype,
-          body: fs.createReadStream(each.path),
-        },
-        supportsAllDrives: true,
-        fields: "id, name",
-      });
+          size: each.size,
+          type: "document",
+          user: userId,
+        });
 
-      allFiles.push({url: response.data.id, name: response.data.name, mimeType: each.mimetype, size: each.size, type: "document", user: userId})
-
-      fs.unlinkSync(each.path);
-
-
-      
+        fs.unlinkSync(each.path);
+      }
     }
-  }
 
- 
+    // console.log(photoIds)
+    // console.log(videoIds)
 
+    // console.log(documentIds)
 
+    console.log(allFiles);
 
+    // const allUsers = await User.find()
+    // const currentUser = await User.updateOne({_id: user}, {files: allFiles})
+    const insertFiles = await File.insertMany(allFiles);
 
-  // console.log(photoIds)
-  // console.log(videoIds)
-
-  // console.log(documentIds)
-
-  console.log(allFiles)
-
-  // const allUsers = await User.find()
-  // const currentUser = await User.updateOne({_id: user}, {files: allFiles})
-  const insertFiles = await File.insertMany(allFiles)
-
-  // console.log(allUsers)
-  console.log(insertFiles)
-
-
-
-
-
-
-  
+    // console.log(allUsers)
+    console.log(insertFiles);
 
     // fs.unlinkSync(req.file.path);
 
@@ -311,7 +261,7 @@ const allFiles = []
       // fileName: response.data.name,
     });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     console.error("Upload failed:", err.message);
 
     // if (fs.existsSync(req.file.path)) {
@@ -324,82 +274,50 @@ const allFiles = []
   }
 });
 
-
-app.get('/allfiles/:userId', async (req, res) => {
-
-  const userId = req.params.userId
+app.get("/allfiles/:userId", async (req, res) => {
+  const userId = req.params.userId;
 
   try {
-
-    const allFiles =  await File.find({user: userId}, 
+    const allFiles = await File.find(
+      { user: userId }
       // {url: 1, user: 1}
-    )
+    );
 
-    res.json({allFiles})
-
-    
-
-    
-
-
+    res.json({ allFiles });
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-})
+});
 
+app.get("/files/:fileId", async (req, res) => {
+  if (req.cookies) {
+    // console.log("cookie", req.cookies)
+    // console.log("session", req.cookies.session)
+    req.user = jwt.verify(req.cookies.session, process.env.ACCESS_TOKEN_SECRET);
+    console.log(req.user);
+    let requestingUser = await User.findById(req.user.id);
+    console.log(requestingUser);
+  }
+  // else return
 
-app.get('/files/:fileId', async (req, res) => {
+  const requestedFile = req.params.fileId;
 
-if (req.cookies)   {
-  // console.log("cookie", req.cookies)
-  // console.log("session", req.cookies.session)
-  req.user = jwt.verify(req.cookies.session, process.env.ACCESS_TOKEN_SECRET)
-  console.log(req.user)
-  let requestingUser = await User.findById(req.user.id)
-  console.log(requestingUser)
-
-} 
-// else return
-
-  
-
-
-
-
-  
-
-
-
-  
-
-  const requestedFile = req.params.fileId
-
-  console.log(requestedFile)
+  console.log(requestedFile);
 
   try {
+    const driveStream = await drive.files.get(
+      {
+        fileId: requestedFile,
+        alt: "media",
+      },
+      { responseType: "stream" }
+    );
 
-    const driveStream = await drive.files.get({
-      fileId: requestedFile, alt: 'media'
-    },
-    { responseType: 'stream'}
-  )
-
-  driveStream.data.pipe(res)
-
-
-    
-
-    
-
-
+    driveStream.data.pipe(res);
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-})
-
-
-
-
+});
 
 const port = 3603;
 app.listen(port, () => {
